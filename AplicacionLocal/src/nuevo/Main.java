84ad905e4314a3787d1fbbc3d2eed9ddaffa3433
begoloplor <begoloplor@gmail.com>
2019-04-09 01:20:16 +0200

    ewew

--- AplicacionLocal/src/nuevo/Main.java
+++ AplicacionLocal/src/nuevo/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 //Esto es el master
-=======
+
 //Solo nuevo
->>>>>>> refs/remotes/origin/Nuevo
+
 	}
 
 }
