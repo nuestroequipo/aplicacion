199262908f6ccd76bc95a2f187eb9aa1ba8ca4b1
begoloplor <begoloplor@gmail.com>
2019-04-11 01:28:34 +0200

    Cambios

--- AplicacionLocal/src/nuevo/Main.java
+++ AplicacionLocal/src/nuevo/Main.java
@@ -3,9 +3,7 @@ package nuevo;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-// Este código quiero que se quede
+	
 		
 
 	}
--- AplicacionLocal/src/nuevo/Prueba.java
+++ AplicacionLocal/src/nuevo/Prueba.java
@@ -3,4 +3,5 @@ package nuevo;
 public class Prueba {
 	private int num;
 	private String cad;
+	// ESto es del ultimo
 }
